3050834384056f8bcbd66ccfd8d905650c54a040
walterYangana <medinawalter3@gmail.com>
2022-02-07 17:17:23 -0500

    modificando ip y puerto

--- app/src/main/java/app/utiltek/udemydelivery/api/ApiRoutes.kt
+++ app/src/main/java/app/utiltek/udemydelivery/api/ApiRoutes.kt
@@ -3,7 +3,7 @@ package app.utiltek.udemydelivery.api
 import app.utiltek.udemydelivery.routes.UserRoutes
 //hace las veces del server en node para llamar las rutas
 class ApiRoutes {
-    val API_URL="http://192.168.10.11:3000/api/"
+    val API_URL="http://10.142.11.251:8080/api/"
     val retrofit=RetrofitClient()
 
     //contiene las rutas del usuario
